97d623c3bdffdae80bc4e7552d50deb4fe567e7c
MINJUN KWAK <kreateark@gmail.com>
2017-10-07 11:31:42 +0900

    GUI added, facebook status added
    app_id and app_secret required

--- main.py
+++ main.py
@@ -1,12 +1,159 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import feedparser
 import urllib.request as req
+import urllib.request
 import re
 import datetime
+import json;  import csv
+from datetime import date,timedelta; import time;
+
+app_id = ""
+app_secret = ""
+access_token = app_id + "|" + app_secret
+page_id = "206910909512230"
+since = str(date.today()-timedelta(1))
+until = str(date.today())
 
 
+# advaced information
+def getFacebookPageFeedData(page_id, access_token, since, unitl):
+    # construct the URL string
+    # print("get Facebook Page Feed Data() is called")
+    base = "https://graph.facebook.com"
+    node = "/" + page_id + "/feed"
+    parameters1 = "/?fields=message,created_time,likes.limit(1).summary(true),"
+    # -b - cf -  comments.fields(message,parent).summary(true) (- cannot see replies)
+    # -b - changed if you add parent in  filter(stream){message,id,"parent"}, you can see parent
+    parameters2 = "comments.summary(true).filter(stream){message,like_count}"
+    time = "&since=%s&until=%s" % (since, until)
+    access = "&access_token=%s" % access_token
+    url = base + node + parameters1 + parameters2 + time + access
+    # print(url)  ###DEL
+
+    # retrieve data
+    data = json.loads(request_until_suceed(url))
+
+    return data
+
+
+def request_until_suceed(url):
+    req = urllib.request.Request(url)
+    success = False
+    while success is False:
+        try:
+            response = urllib.request.urlopen(req)
+            if response.getcode() == 200:
+                success = True
+        except Exception as e:
+            # print(e)  # wnat to know what error it is
+            time.sleep(5)
+            # print("Error for url %s : %s" % (url, datetime.datetime.now()))
+
+    return response.read().decode(response.headers.get_content_charset())
+
+
+def fetch_comments(status, status_message):
+    # print("fetch comments is called")
+    com = status_message
+    page = ' ' if 'comments' not in status.keys() else status['comments']
+    # print json.dumps(page, indent=4, sort_keys=True) ###DELETE
+    j = 0  ################DELETE
+    while True:  # until no more next
+        try:
+
+            comments = ' ' if 'comments' not in status.keys() else page['data']
+            ##########until no more comment in page
+            i = 0
+            while True:
+
+                try:
+                    # append message and comments using :]
+                    # http://me2.do/5ZryZrRd(not considering codec error
+
+                    like_count = comments[i]['like_count']
+
+                    if like_count > 10:
+                        com = com + ' :] ' + comments[i]['message'].encode('cp949', errors='replace').decode('cp949')
+                        # print("like_count : %s"%like_count)
+                    i = i+1
+                except:
+                    break
+                    ############
+
+            # get next page comment json
+            nex = json.loads(request_until_suceed(page['paging']['next']))
+            page = nex
+            j = j + 1;
+            #print("   %d th comment in one status" % j)
+            # print json.dumps(page, indent=4, sort_keys=True)###DELETE
+
+        except KeyError:  # no more next
+            break
+
+    return com
+
+
+def processFacebookPageFeedStatus(status):
+    # key is the name of the list
+    status_message = ' ' if 'message' not in status.keys() else status['message'].encode('cp949',
+                                                                                         errors='replace').decode(
+        'cp949')
+    # time(http://devanix.tistory.com/306)
+    status_published = datetime.datetime.strptime(status['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
+    status_published = status_published + datetime.timedelta(hours=+9)
+    status_published = status_published.strftime('%Y-%m-%d %H:%M:%S')
+
+    num_likes = 0 if 'likes' not in status.keys() else status['likes']['summary']['total_count']
+    num_comments = 0 if 'comments' not in status.keys() else status['comments']['summary']['total_count']
+    com = fetch_comments(status, status_message)
+
+    return (status_message, status_published, num_likes, com)
+
+
+def fetch_feed():
+    # print("I am string")
+    print("대나무 숲은 데이터를 긁어오는데 시간이 걸립니다. 조금만 기다려주세요")
+    one_json = getFacebookPageFeedData(page_id, access_token, since, until)
+    wan_data = ""
+    j = 0
+    i = 0
+    num = 0
+    while True:
+        try:
+            test_status = one_json["data"][i]
+            num_likes = test_status["likes"]["summary"]["total_count"]
+
+            if num_likes > 200 :
+                processed_test_status = processFacebookPageFeedStatus(test_status)
+                # wan_data.append(list(processed_test_status))
+                wan_data+=str(processed_test_status)
+                # print("%d th status in %d" % (i, num))
+                num = num + 1
+            i = i + 1
+        except Exception as e:
+            # print(e)
+            try:
+                next_url = one_json["paging"]["next"]  # next url
+                print(next_url)
+                j = j + 1
+                print("----")
+                # print j #FOR CHECK
+                one_json = json.loads(request_until_suceed(next_url))
+                i = 0
+                continue
+            except KeyError:
+                # print('End of Document')
+                break
+        # print(wan_data)
+    # print("get out")
+    return str(wan_data)
+
+# 뉴스 전문 파싱
 def get_text(URL):
   source_code_from_URL = req.urlopen(URL)
   soup = BeautifulSoup(source_code_from_URL, 'lxml', from_encoding='utf-8')
@@ -93,9 +240,13 @@ def writeletter(type):
         soup = BeautifulSoup(res, "html.parser")
         news += soup.select("title,description")
 
+    elif type ==8:
+        news=fetch_feed()
+        print("before")
+
 
     for a in news:
-        if type == 4 or type ==7 :
+        if type == 4 or type ==7 or type ==8 :
             b = a
         else:
             b = a.string
@@ -107,7 +258,7 @@ def writeletter(type):
                 totalcharacter = 0
                 contents.append("")
 
-            if type == 4 or type ==7:
+            if type == 4 or type ==7 or type==8:
 
                 # 육군훈련소의 인터넷 편지는 줄바꿈이 인식이 되지않는다.
                 if (b == "\n"):
@@ -121,6 +272,8 @@ def writeletter(type):
 
 
 def sendletter(name, birthday, enrollmentdate, type):
+    print("자동화된 크롬창을 건들면 프로시져가 취소됩니다.")
+    print("휴대폰 인증이 뜨면 인증을 해주세요")
     writeletter(type)
     driver = webdriver.Chrome()
     # 크롬 창 최대화를 통해 에러제거
@@ -157,7 +310,7 @@ def sendletter(name, birthday, enrollmentdate, type):
     elif type == 3:
         today += " CNN 뉴스"
     elif type == 4:
-        today += "[군대얘기] 제 군생활에서 만난 참군인 대대장님 썰.txt"
+        today += "텍스트파일.txt"
     elif type == 5:
         today += " 중앙일보 연예 뉴스"
     elif type == 6:
@@ -166,11 +319,13 @@ def sendletter(name, birthday, enrollmentdate, type):
         today += " JTBC 탑텐 뉴스 전문"
     elif type == 0:
         today += " 중앙일보 기본, 연예, 스포츠 뉴스"
+    elif type ==8:
+        today+=" 고려대학교 대나무숲"
     title = today
 
     # 크롬창 알림 제거
     alert = driver.switch_to_alert()
-
+    print("편지 작성이 시작됩니다. 크롬창을 가만히 두세요")
     # 편지작성(글자수에 따른 분할)
     for i in range(0, numberofpages + 1):
         driver.find_element_by_css_selector("#article_title").send_keys(title + str(i + 1))
@@ -194,14 +349,133 @@ contents = [""]
 enrollmentdate = "20170904"
 name = "이인석"
 birthday = "940223"
-type = 2
+type = 7
 
-enrollmentdate = input("입대일을 입력하세요 (ex:20170904)\n입대일: ")
-name = input("훈련병의 이름을 입력하세요 (ex:이인석)\n이름: ")
-birthday = input("훈련병의 생일을 입력하세요 (ex: 940223)\n생일: ")
-type = int(input("보내실 편지의 내용을 정해주세요(0: 중앙일보 기본,연예,스포츠, 1:JTBC 뉴스, 2:중앙일보 뉴스, 3: CNN, 4:텍스트파일, 5:중앙일보 연예, 6:중앙일보 스포츠) 숫자만 입력해주세요\n숫자:"))
-print("핸드폰 인증이 나올때까지 아무것도 건드리지 말아주세요ㅠㅜ")
 
-sendletter(name, birthday, enrollmentdate, type)
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
 
-# 내가 훈련소에 있었을때 매일밤 이 모든 작업을 수작으로 하였던 내 아버지에게 바칩니다.
+    def setupUI(self):
+        self.setGeometry(800, 400, 350, 350)
+
+        # Label
+        soldiername = QLabel("훈련병 이름", self)
+        soldiername.move(20, 25)
+
+        enlistmentdate = QLabel("입대일", self)
+        enlistmentdate.move(20, 60)
+
+        soldierbirthdate = QLabel("훈련병 생일", self)
+        soldierbirthdate.move(20, 95)
+
+        soldiername = QLabel("(예: 이인석)", self)
+        soldiername.move(200, 25)
+
+        enlistmentdate = QLabel("(예: 20170904)", self)
+        enlistmentdate.move(200, 60)
+
+        soldierbirthdate = QLabel("(예: 940223)", self)
+        soldierbirthdate.move(200, 95)
+
+        # LineEdit
+        self.lineEditName = QLineEdit("", self)
+        self.lineEditName.move(90, 25)
+        self.lineEditName.textChanged.connect(self.lineEditChanged)
+
+        self.lineEditEnlistmentDate = QLineEdit("", self)
+        self.lineEditEnlistmentDate.move(90, 60)
+        self.lineEditEnlistmentDate.textChanged.connect(self.lineEditChanged)
+
+        self.lineEditSoldierBirthDate = QLineEdit("", self)
+        self.lineEditSoldierBirthDate.move(90, 95)
+        self.lineEditSoldierBirthDate.textChanged.connect(self.lineEditChanged)
+
+        # StatusBar
+        self.statusBar = QStatusBar(self)
+        self.setStatusBar(self.statusBar)
+
+        self.radio1 = QRadioButton("JTBC Top 10 전문", self)
+        self.radio1.move(20, 125)
+        self.radio1.setChecked(True)
+        self.radio1.clicked.connect(self.radioButtonClicked)
+        self.radio1.resize(200,30)
+
+        self.radio2 = QRadioButton("중앙일보 기본, 연예, 스포츠 요약", self)
+        self.radio2.move(20, 145)
+        self.radio2.clicked.connect(self.radioButtonClicked)
+        self.radio2.resize(200, 30)
+
+        self.radio3 = QRadioButton("텍스트 파일", self)
+        self.radio3.move(20, 165)
+        self.radio3.clicked.connect(self.radioButtonClicked)
+
+        self.radio4 = QRadioButton("CNN 요약", self)
+        self.radio4.move(20, 185)
+        self.radio4.clicked.connect(self.radioButtonClicked)
+
+        self.radio5 = QRadioButton("고대 대나무숲", self)
+        self.radio5.move(20, 205)
+        self.radio5.clicked.connect(self.radioButtonClicked)
+
+        textLabel = QLabel("Message:  ", self)
+        textLabel.move(20, 225)
+
+        self.label = QLabel("", self)
+        self.label.move(80, 225)
+        self.label.resize(300, 30)
+
+        btn1 = QPushButton("보내기", self)
+        btn1.move(200, 265)
+        btn1.clicked.connect(self.btn1_clicked)
+
+
+        self.statusBar = QStatusBar(self)
+        self.setStatusBar(self.statusBar)
+
+    def lineEditChanged(self):
+        global name,enrollmentdate,birthday
+        name=self.lineEditName.text()
+        enrollmentdate=self.lineEditEnlistmentDate.text()
+        birthday=self.lineEditSoldierBirthDate.text()
+
+
+    def radioButtonClicked(self):
+        msg = ""
+        global type
+        if self.radio1.isChecked():
+            msg = "JTBC 뉴스 Top 10 전문"
+            type=7
+
+        elif self.radio2.isChecked():
+            msg = "중앙일보 기본, 연예, 스포츠 요약"
+            type=0
+
+        elif self.radio3.isChecked():
+            msg="텍스트 파일"
+            type=4
+
+        elif self.radio4.isChecked():
+            msg= "CNN 요약"
+            type=3
+
+        elif self.radio5.isChecked():
+            msg = "고대 대나무숲"
+            type=8
+
+        self.statusBar.showMessage(msg + " 선택 됨")
+
+    def btn1_clicked(self):
+        self.label.setText("핸드폰 인증이 뜰때까지 아무것도 건드리지마세요")
+        global name,birthday,enrollmentdate, type
+        sendletter(name,birthday,enrollmentdate, type)
+
+    def btn2_clicked(self):
+        self.label.clear()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mywindow = MyWindow()
+    mywindow.show()
+    app.exec_()
