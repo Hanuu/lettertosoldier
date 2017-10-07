f3efae6a2af0625cc7eddf24ec71c77fd960d237
tycheyoung <retbird13@gmail.com>
2017-10-07 18:50:40 +0900

    MacOS Support

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def sendletter(name, birthday, enrollmentdate, type):
     print("자동화된 크롬창을 건들면 프로시져가 취소됩니다.")
     print("휴대폰 인증이 뜨면 인증을 해주세요")
     writeletter(type)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome('./chromedriver')
     # 크롬 창 최대화를 통해 에러제거
     driver.maximize_window()
 
